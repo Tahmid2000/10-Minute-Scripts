d50003e18bdcd19c21903fd5427d6d1b7c2c813d
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-26 10:59:37 -0600

    delete spotify

--- discover_all.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-headers = {'Accept': 'application/json', 'Content-Type': 'application/json',
-           "Authorization": "Bearer "}
-url = 'https://api.spotify.com/v1/artists//albums'
-data1 = requests.get(url, headers=headers).json()
-print(data1)
